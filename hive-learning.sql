d2b7c5b620bb885eb217f8847aed495b4dbcdf3f
xiaozhch5 <xiaozhch5@mail2.sysu.edu.cn>
2019-07-14 15:20:33 +0800

    update the hive-learning.sql file

--- hive-learning.sql
+++ hive-learning.sql
@@ -1,6 +1,10 @@
 /***************************************
  * hive基本操作方法
- * 
+ * warming:
+ * Hive-on-MR is deprecated in Hive 2 and may not be available in the future versions. 
+ * Consider using a different execution engine (i.e. spark, tez) or using Hive 1.X releases.
+ * hive-on-MR真的好慢啊（小声bb
+ ）
  * @author zhchxiao
  * @date 2019/7/13
  ***************************************/
@@ -87,29 +91,36 @@ provinceid int);
 --删除表
 drop table if exists user_info;
 
--------------------------------------------------
+----------------------------------------------------------
 
---向hive中加载数据
+--向hive中加载数据（在原始创建的表中加载数据，即userinfo表）
+/*
+ *新建一个userinfo.txt文件，文件内容如下(行内用'\t'分隔)
+1	zhangsan	111	2019-04-11
+2	lisi	112	2019-04-12
+3	wangwu	113	2019-04-13
+4	zhaoliu	113	2019-04-14
+*/
 ---将本地文本文件内容批量加载带hive中，要求文本文件中的格式和hive表中的定义一致，
 ---包括字段个数、字段顺序、列分隔符都要一致
 ---local表示数据源文件在本地，如果数据源在hdfs上则去掉local，文件路径用'hdfs://namenode:9000/'表示
 ---overwrite表示如果hive中存在数据，则会覆盖掉原有的数据，如果省略则表示追加
-load data local inpath '/home/vadmin/user_info_data.txt' overwrite into table user_info;
+load data local inpath '/home/vadmin/user_info_data.txt' overwrite into table userinfo;
 
 --加载到分区表
-load data local inpath '/home/vadmin/user_info_data.txt' overwrite into table user_info partition (dt='2019-5-5');
+load data local inpath '/home/vadmin/user_info_data.txt' overwrite into table userinfo partition (dt='2019-05-05');
 
 --加载到分桶表
 ---数据载入临时表
-load data local inpath '/home/vadmin/user_info_data.txt' overwrite into table user_info;
+load data local inpath '/home/vadmin/user_info_data.txt' overwrite into table userinfo;
 ---导入分桶表
 ---set hive.enforce.bucketing = true; 这个配置非常关键，为true就是设置为启用分桶。
 set hive.enforce.bucketing = true;
-insert overwrite table user_lead_info select * from user_info;
+insert overwrite table user_lead_info select * from userinfo;
 
 --从hive导出数据
 ---去掉local可导出到hdfs
-insert overwrite local directory 'home/vadmin/user_info.bak2019-5-5' select * from user_info;
+insert overwrite local directory '/home/vadmin/user_info.bak2019-5-5' select * from userinfo;
 
 --插入数据到hive中
 insert overwrite table user_leads select * from  user_leads_tmp;
@@ -141,5 +152,52 @@ to '/user/hive/action_log.export'
 ---将备份在HDFS上的文件，还原到user_action_log_like表中。
 import table user_action_log_like from '/user/hive/action_log.export';
 
--------------------------------------------------
+---------------------------------------------------------------
+-----------------------  HQL语法  -----------------------------
+--select查询
+select * from user_leads;
+select leads_id, user_id, create_time from user_leads;
+select e.leads_id from user_leads e;
 
+---函数列
+---hive可以使用自带函数也可以使用自定义函数
+select companyid, upper(host), UUID(32) from user_action_log;
+
+---算数运算列
+select companyid, userid, (companyid+userid) as sumint from user_action_log;
+
+---限制返回条数
+select * from user_action_log limit 100;
+
+---case when then语句
+select case companyid when 0 then '未登录' else companyid end from user_action_log;
+
+---where筛选
+---where后面接一个条件语句
+select * from user_leads where create_time<'2016-08-22';
+
+---group by分组
+
+---子查询
+---hive对子查询的支持有限，只允许在select from后面出现
+select * from 
+(select  userid, username from user_info i where i.userid='10595');
+
+--hive join操作
+---hive只支持等值连接，不支持不等值连接
+---inner join
+---left join
+---right join
+---full join
+---left semi-join
+
+--排序
+---order by
+---按照某一列排序，如果数据量大，会造成reduce执行相当漫长
+select * from user_leads order by userid;
+
+--sort by
+
+--distribute by 和 sort by
+
+--cluster by
